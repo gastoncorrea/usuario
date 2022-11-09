ac916f049889ff9f1cebcc05e5e266cae084355e
Gaston <gastoncorrea90e@gmail.com>
2022-11-09 19:10:09 -0300

    se creo directorio componentes y FormUsuario.js

---
+++ src/components/FormUsuario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormUsuario = () => {
+    return (
+        <div>
+            <h1>Formulario para usuario</h1>
+        </div>
+    );
+};
+
+export default FormUsuario;
